5984325ea86306728480da6e98280bdc707860d0
Bob <bo_lv@mtelnet.com>
2016-11-15 13:04:23 +0800

    no message

---
+++ library/src/main/java/com/lvbo/library/First.java
@@ -0,0 +1,14 @@
+package com.lvbo.library;
+
+import android.util.Log;
+
+/**
+ * Created by lvbo on 2016/11/15.
+ */
+
+public class First {
+
+    public static void sayHello(){
+        Log.e("lvbo"," say hello ");
+    }
+}
